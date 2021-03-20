a72f2f0a15b264c11d272f9d26c21817f54b13e9
sugarycoke <58714552+sugarycoke@users.noreply.github.com>
2021-03-19 23:16:22 -0400

    data thing updated

--- data.js
+++ data.js
@@ -1,18 +1,43 @@
 const csv = require('csvtojson');
-const filePath = 'VaccinationsDaily.csv';
-let data = [];
-let labels = [];
+const got = require('got');
 
-async function getData() {
-    const jsonObj = await csv().fromFile(filePath);
-    // console.log(jsonObj.slice(-7));
-    json = jsonObj.slice(-7);
-    for(item in json) {
-        data.push(json[item]['CompletedVaxCumulative']);
-        labels.push(json[item]['VACCINATION_DATE']);
+
+class Data {
+    constructor() {
+        this.data = [];
+        this.labels = [];
+    }
+    async getData() {
+        var response = await got.get('https://state-of-maryland.github.io/VaccineDashboardGraphs/Data/VaccinationsDaily.csv', {
+            headers: {
+                'user-agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
+                'x-forwarded-for': '66.249.66.1',
+            }
+        });
+        // console.log(response.body);
+        const jsonObj = await csv().fromString(response.body);
+        // console.log(jsonObj.slice(-7));
+        let json = jsonObj.slice(-7);
+        for(var i=0; i<json.length; i++) {
+            this.data.push(json[i]['CompletedVaxCumulative']);
+            this.labels.push(json[i]['VACCINATION_DATE']);
+        };
+
+        return {labels: this.labels, data: this.data};
     };
+}
 
-    console.log(data, labels);
-};
+module.exports = {
+    Data: Data
+}
 
-getData();
+/*
+
+const Task = new Data();
+
+(async () => {
+    const thing = await Task.getData();
+    console.log(thing);
+})();
+
+*/
