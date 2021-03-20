44d696b60e9d0a1bc7d098317e38793bd4e4a25b
NickFet <73346471+NickFet@users.noreply.github.com>
2021-03-20 02:48:29 -0400

    tialwind moduels

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'navblue': '#202541',
+        'bgblue': '#121524',
+        'highlightblue': '#8A9CFF',
+        'darkblue': '#161A2C',
+        'headerblue': '#192041',
+        'fillergray': '#50546A',
+        "linkblue": '#4760E8',
+      },
+      fontFamily: {
+        body: ['Montserrat']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
