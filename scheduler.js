1e9ef54a37804d24b2534cffa299028c8b3fd5f8
Adam <ghostlyvisuals@gmail.com>
2021-03-20 21:21:20 -0400

    Update scheduler.js

--- scheduler.js
+++ scheduler.js
@@ -1,9 +1,62 @@
 const got = require("got");
 const { CookieJar } = require("tough-cookie");
+const open = require("open");
+const qs = require("qs");
+const Captcha = require("2captcha")
 let enabled = false;
+let date2;
+let time;
+let currentRunning = {};
+const CaptchaTask = new CaptchaGen();
+CaptchaTask.generateCaptchas();
+class CaptchaGen {
+    constructor() {
+        this.captchaList = [];
+    }
+    async sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+    async generateCaptchas() {
+        const solver = new Captcha.Solver("d7a73c14085dc2c6f579e70548f7a6c4");
+        while(true) {
+            solver.recaptcha("6LffjDEaAAAAAB36hjaQJmIkZTNDrpyjDMugfvZt", "https://signupandschedule.umm.edu/mychart/SignUpAndSchedule/EmbeddedSchedule?id=RES^84002860&VT=22759")
+            .then(async (res) => {
+                this.captchaList.push(res.data);
+                await this.sleep(40000);
+                var index = this.captchaList.indexOf(res.data);
+                this.captchaList.splice(index, 1);
+            }).catch(error => {
+                console.log("Error with captcha...")
+            });
+            await this.sleep(10000)
+        }
+    }
+    async grabCaptcha() {
+        if(this.captchaList.length > 0) {
+            var temp = this.captchaList[0];
+            this.captchaList.splice(0,1);
+            return temp;
+        } else {
+            return "NoCaptcha";
+        }
+    }
+}
+class ScheduleChecker {
+    constructor(phone) {
+        this.phone = phone;
+    }
+    async check() {
+        if(currentRunning[this.phone]) {
+            return currentRunning[this.phone].status;
+        } else {
+            return "Error"
+        }
+    }
+}
 class ScheduleMT {
     constructor(info) {
         this.info = info;
+        currentRunning[this.info.phone] = {fullName:this.info.fName+" "+this.info.lName,status: false}
     }
     async sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
@@ -31,10 +84,10 @@ class ScheduleMT {
                 /*for(var j = 0; j < Object.keys(status.mtBank.locations).length; j++) {
                     status.mtBank.locations[Object.keys(status.mtBank.locations)[j]] = undefined;
                 }*/
-                for(var n = 0; n < 5; n++) {
+                for(var n = 0; n < 1; n++) {
                     var response2 = await got.post("https://signupandschedule.umm.edu/MyChart/OpenScheduling/OpenScheduling/GetOpeningsForProvider?noCache=0.14464521336546898", {
                         headers: {
-                            'user-agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
+                            'user-agent': 'Mozilla/5.0 (Linux; Android 5.0; SM-G920A) AppleWebKit (KHTML, like Gecko) Chrome Mobile Safari (compatible; AdsBot-Google-Mobile; +http://www.google.com/mobile/adsbot.html)',
                             'x-forwarded-for': '66.249.66.1',
                             '__RequestVerificationToken': securityToken,
                             "Origin": "https://signupandschedule.umm.edu",
@@ -52,22 +105,117 @@ class ScheduleMT {
                     });
                     try {
                         date = JSON.parse(response2.body)["LatestDate"]
-                        for(var i = 0; i < Object.keys(JSON.parse(response2.body)["AllDays"]).length; i++) {
+                        var i = 0;
+                        if(Object.keys(JSON.parse(response2.body)["AllDays"]).length>0) {
                             if(!enabled) {
-                                console.log(JSON.parse(response2.body)["AllDays"][Object.keys(JSON.parse(response2.body)["AllDays"])[i]]);
+                                console.log(JSON.parse(response2.body)["AllDays"][Object.keys(JSON.parse(response2.body)["AllDays"])[0]].DateISO);
+                                date2 = JSON.parse(response2.body)["AllDays"][Object.keys(JSON.parse(response2.body)["AllDays"])[0]].DateISO;
+                                time = JSON.parse(response2.body)["AllDays"][Object.keys(JSON.parse(response2.body)["AllDays"])[0]].Slots[0].StartTimeISO;
+                                var captchaToken = await CaptchaTask.grabCaptcha();
+                                if(captchaToken == "NoCaptcha") {
+                                    console.log("No captcha available...");
+                                } else {
+                                    var response3 = await got.post("https://signupandschedule.umm.edu/mychart/OpenScheduling/OpenScheduling/CreatePreloginSession", {
+                                        headers: {
+                                            'user-agent': 'Mozilla/5.0 (Linux; Android 5.0; SM-G920A) AppleWebKit (KHTML, like Gecko) Chrome Mobile Safari (compatible; AdsBot-Google-Mobile; +http://www.google.com/mobile/adsbot.html)',
+                                            'x-forwarded-for': '66.249.66.1',
+                                            '__RequestVerificationToken': securityToken,
+                                            'Content-Type': "application/x-www-form-urlencoded; charset=UTF-8",
+                                            "X-Captcha-Response": captchaToken
+                                        },
+                                        body: "accessCode=&userEnteredDateOfBirth=&sourceToken=",
+                                        cookieJar: this.cookieJar
+                                    });
+                                    var sessionToken = JSON.parse(response3.body).SessionToken;
+                                    var response4 = await got.post("https://signupandschedule.umm.edu/mychart/OpenScheduling/OpenScheduling/MakeReservation", {
+                                        headers: {
+                                            'user-agent': 'Mozilla/5.0 (Linux; Android 5.0; SM-G920A) AppleWebKit (KHTML, like Gecko) Chrome Mobile Safari (compatible; AdsBot-Google-Mobile; +http://www.google.com/mobile/adsbot.html)',
+                                            'x-forwarded-for': '66.249.66.1',
+                                            '__RequestVerificationToken': securityToken,
+                                            'Content-Type': "application/x-www-form-urlencoded; charset=UTF-8",
+                                            "X-Requested-With": "XMLHttpRequest"
+                                        },
+                                        body: qs.stringify({
+                                            "providerIdList[0]": "RES^84002860",
+                                            "baseProviderId": "RES^84002860",
+                                            "departmentId": "2010017206",
+                                            "visitTypeId": "22759",
+                                            "date": date2,
+                                            "startTime": time,
+                                            "duration": "15",
+                                            "clientTimeZoneOffset": "240",
+                                            "sessionToken": sessionToken
+                                        }),
+                                        cookieJar: this.cookieJar
+                                    });
+                                    console.log("Session created...");
+                                    if(JSON.parse(response4.body).CanUseSlot == false) {
+                                        console.log("Too late");
+                                    } else {
+                                        var reservationKey = JSON.parse(response4.body).ReservationKey;
+                                        var response5 = await got.post("https://signupandschedule.umm.edu/mychart/OpenScheduling/SignupAndSchedule/Signup?noCache=0.9851966613756604", {
+                                            headers: {
+                                                'user-agent': 'Mozilla/5.0 (Linux; Android 5.0; SM-G920A) AppleWebKit (KHTML, like Gecko) Chrome Mobile Safari (compatible; AdsBot-Google-Mobile; +http://www.google.com/mobile/adsbot.html)',
+                                                'x-forwarded-for': '66.249.66.1',
+                                                '__RequestVerificationToken': securityToken,
+                                                'Content-Type': "application/x-www-form-urlencoded; charset=UTF-8",
+                                                "X-Requested-With": "XMLHttpRequest"
+                                            },
+                                            body: qs.stringify({
+                                                "FirstName": this.info.fName,
+                                                "MiddleName": "",
+                                                "LastName": this.info.lName,
+                                                "Gender": this.info.gender,
+                                                "DateOfBirthStr": this.info.dob,
+                                                "Race": this.info.race,
+                                                "Ethnicity": "-1",
+                                                "PreferredLanguage": "-1",
+                                                "NationalId": "",
+                                                "AddressLine1": this.info.address1,
+                                                "AddressLine2": "",
+                                                "City": this.info.city,
+                                                "State": "21",
+                                                "PostalCode": this.info.zip,
+                                                "HomePhone": this.info.phone,
+                                                "WorkPhone": "",
+                                                "MobilePhone": "",
+                                                "Email": "",
+                                                "SupportsAccountCreation": "False",
+                                                "DuplicateResult": "",
+                                                "DepartmentIdForPatientCreate": "2010017206",
+                                                "AppointmentProviderId": "RES^84002860",
+                                                "AppointmentVisitTypeId": "22759",
+                                                "AppointmentTime": time,
+                                                "AppointmentDate": date2,
+                                                "ReservationKey": reservationKey,
+                                                "SessionToken": sessionToken
+                                            }),
+                                            cookieJar: this.cookieJar
+                                        });
+                                        console.log(response5.body);
+                                        if(JSON.parse(response5.body).action != "showerrors") {
+                                            enabled = true;
+                                            currentRunning[this.info.phone].status = true;
+                                            return;
+                                        } else {
+                                            console.log("Error filling out form for "+this.info.phone+"...");
+                                        }
+                                    }
+                                }
                             }
                         }
+                        if(Object.keys(JSON.parse(response2.body)["AllDays"]).length == 0) {
+                            enabled = false;
+                        }
                     } catch(e) {
-                        console.log("Error")
+                        //console.log(e)
                     }
                 }
 
-                await this.sleep(5)
+                await this.sleep(3)
             } catch(e) {
-                console.log(e)
+                //console.log("Error 2")
             }
         }
     }
 }
-const Task = new ScheduleMT({})
-Task.monitor();
