3d1f6e7950a15a5c8243d7dffad0fa5b8e91a6e2
sugarycoke <58714552+sugarycoke@users.noreply.github.com>
2021-03-20 21:24:49 -0400

    Update main.js

--- main.js
+++ main.js
@@ -6,13 +6,16 @@ const app = express();
 app.get('/api/graphData', async (req, res) => {
     const Task = new Data();
     const data = await Task.getData();
-    res.send(JSON.stringify(data))
+    res.send(JSON.stringify(data));
 });
 app.get('/api/newsData', async (req, res) => {
     const Task = new News();
     const data = await Task.grabNews();
-    res.send(JSON.stringify(data))
+    res.send(JSON.stringify(data));
+});
+app.get('/', async (req, res) => {
+    res.sendFile('C:/Users/kprsh/Documents/GitHub/mocohacks2021/b.html');
 });
 app.listen(3000, () => {
-    console.log(`Example app listening at http://localhost:${3000}`)
+    console.log(`Example app listening at http://localhost:${3000}`);
 })
